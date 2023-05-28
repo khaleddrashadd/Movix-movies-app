78c835625a710fad0681b54173cbf16daee447cf
khaleddrashadd <khaledrashad111@gmail.com>
2023-05-29 02:36:30 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -30,9 +30,9 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/:mediaType/:id" element={<Details />} />
-        <Route path="/search/:query" element={<SearchResults />} />
-        <Route path="/explore/:mediaType" element={<Explore />} />
+        <Route path=":mediaType/:id" element={<Details />} />
+        <Route path="search/:query" element={<SearchResults />} />
+        <Route path="explore/:mediaType" element={<Explore />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
       <Footer />
